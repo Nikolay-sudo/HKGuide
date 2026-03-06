15d91acbffeadeed4b44037bf4a8fa3b658e4209
virus-source <gegagedigedagedagogg@gmail.com>
2026-03-06 18:39:10 +0100

    два амулета

--- Hollow_knight_Guide/app.py
+++ Hollow_knight_Guide/app.py
@@ -2,29 +2,51 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Расширенная база данных амулетов
+charms_list = [
+    {
+        "id": "compass",
+        "name": "Капризный компас",
+        "slots": 1,
+        "short_desc": "Показывает ваше местоположение на карте.",
+        "full_desc": "Незаменимый инструмент для картографов и исследователей. Позволяет владельцу видеть свое точное местоположение на картах Корнифера.",
+        "how_to_get": "Купите у Изельды в Гряземуте за 220 гео после встречи с Корнифером.",
+        "img_url": "static/images/charms/compass.png",
+        "map_url": "static/images/maps/compass_map.png"
+    },
+    {
+        "id": "mark_of_pride",
+        "name": "Метка гордости",
+        "slots": 3,
+        "short_desc": "Сильно увеличивает дистанцию удара гвоздем.",
+        "full_desc": "Амулет, дарованный племенем богомолов тем, кого они признали достойными. Позволяет наносить удары с гораздо большего расстояния.",
+        "how_to_get": "Находится в сундуке в Деревне Богомолов. Доступно только после победы над Лордами Богомолов.",
+        "img_url": "static/images/charms/mark_of_pride.png",
+        "map_url": "static/images/maps/pride_map.png"
+    }
+]
+
 @app.route('/')
-def index(): return render_template('index.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/charms')
-def charms(): return render_template('charms.html')
+def charms():
+    return render_template('charms.html', charms=charms_list)
 
+# Заглушки для остальных страниц (пока не меняем)
 @app.route('/slots')
-def slots(): return render_template('slots.html')
-
+def slots(): return "В разработке"
 @app.route('/vessels')
-def vessels(): return render_template('vessels.html')
-
+def vessels(): return "В разработке"
 @app.route('/masks')
-def masks(): return render_template('masks.html')
-
-@app.route('/endings')
-def endings(): return render_template('endings.html')
-
-@app.route('/enemies')
-def enemies(): return render_template('enemies.html')
-
+def masks(): return "В разработке"
 @app.route('/abilities')
-def abilities(): return render_template('abilities.html')
+def abilities(): return "В разработке"
+@app.route('/enemies')
+def enemies(): return "В разработке"
+@app.route('/endings')
+def endings(): return "В разработке"
 
 if __name__ == '__main__':
     app.run(debug=True)
